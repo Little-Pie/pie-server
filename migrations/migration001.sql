f79a5b475220f27a80ef11fd778a39c7286c8e3f
Alena <al.venedictova@yandex.ru>
2022-08-17 12:05:29 +0300

    Added migrations

---
+++ migrations/migration001.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users
+  ("id" SERIAL PRIMARY KEY,
+   "name" TEXT NOT NULL,
+   "login" TEXT NOT NULL,
+   "password" TEXT NOT NULL,
+   "data" TIMESTAMPTZ NOT NULL,
+   "admin" BOOLEAN NOT NULL);
+
+--ALTER TABLE <table_name> ADD COLUMN <column_name> <data_type>
+--ALTER TABLE <table_name> DROP COLUMN <column_name>
+--UPDATE <table_name> SET (login,password) = ('pie','123a') WHERE id = 1
