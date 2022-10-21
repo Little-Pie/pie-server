ed12d78e3dfd5e97ef8866ee6cab0c96b81da44e
Alena <al.venedictova@yandex.ru>
2022-10-21 17:47:19 +0400

    Added getArgs - arg "f" initialize filling tables in DB with fixtures

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,14 @@
 module Main where
 
 import Config (Config (..), Environment (..), getConfig)
-import Database.PostgreSQL.Simple (close, connect)
+import Database.PostgreSQL.Simple (Connection, close, connect)
 import Database.PostgreSQL.Simple.Migration (MigrationCommand (..), MigrationContext (..), runMigration)
 import DbQuery.Test (deleteFromTables, dropTables, fillTables)
 import Hash (makeStringHash)
 import Helpers (localPG, printDebug, printError, printRelease, printWarning, withLogging)
 import Network.Wai.Handler.Warp (run)
 import Routing (application)
+import System.Environment (getArgs)
 import System.IO (IOMode (..), hClose, openFile)
 
 main :: IO ()
@@ -22,17 +23,15 @@ main = do
       conn <- connect $ localPG config
       logHandle <- openFile "logFile.txt" AppendMode
       let env = Environment limit offset conn logHandle loggingLevel
-      --dropTables conn
-      --deleteFromTables conn
-      execMigrations env
-      --fillTables conn
+      args <- getArgs
+      runMigrations args conn
       printError env "Serving..."
       run 4000 $ withLogging $ application env
       hClose logHandle
       close conn
 
-execMigrations :: Environment -> IO ()
-execMigrations Environment {..} = do
+execMigrations :: Connection -> IO ()
+execMigrations conn = do
   schemaRes <-
     runMigration $
       MigrationContext MigrationInitialization True conn
@@ -40,3 +39,10 @@ execMigrations Environment {..} = do
     runMigration $
       MigrationContext (MigrationDirectory "migrations") True conn
   pure ()
+
+runMigrations :: [String] -> Connection -> IO ()
+runMigrations ["f"] conn = do
+  dropTables conn
+  execMigrations conn
+  fillTables conn
+runMigrations _ conn = execMigrations conn
