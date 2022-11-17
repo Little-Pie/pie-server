ecf2f03d290af2bfec015c5b7428cfa4f564067f
Alena <al.venedictova@yandex.ru>
2022-11-17 16:21:39 +0400

    Deleted commants from migrations

--- migrations/migration001.sql
+++ migrations/migration001.sql
@@ -6,9 +6,3 @@ CREATE TABLE IF NOT EXISTS users
    "createdAt" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
    "isAdmin" BOOLEAN NOT NULL,
    "isAuthor" BOOLEAN NOT NULL);
-
---ALTER TABLE <table_name> DROP COLUMN <column_name>
---UPDATE <table_name> SET (login,password) = ('pie','123a') WHERE id = 1
---select * from users limit 10 offset 3;
---SELECT * FROM posts JOIN images ON images."postId" = posts.id WHERE posts.id = 1;
---INSERT INTO users ("name","login","password","isAdmin","isAuthor") VALUES ('admin','admin','123',true,true);
